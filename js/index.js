c0c63e72ec90c8e3de906e838f22ef69d1daf75e
Aleu <apahoquei@gmail.com>
2024-10-27 22:54:10 +0100

    .

--- js/index.js
+++ js/index.js
@@ -301,12 +301,8 @@ const updateActiveSection = () => {
 
   navLi.forEach((li) => {
     $(li).removeClass("active").find("a").blur();
-    $(li).find("a").removeClass("active");
-    $(li).find("a").removeAttr("aria-current");
     if (li.classList.contains(activeSection)) {
       li.classList.add("active");
-      $(li).find("a").addClass("active");
-      $(li).find("a").attr("aria-current", "page");
     }
   });
 };
