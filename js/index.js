8c7133b282b352ea8fcc7a98240d40a42255e120
Aleu <apahoquei@gmail.com>
2024-10-28 22:31:34 +0100

    .

--- js/index.js
+++ js/index.js
@@ -338,15 +338,6 @@ $('#menu').on('hide.bs.collapse', () => {
 /* */
 
 /* Links */
-document.querySelectorAll("a[href^='#']").forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
-
 $(".social-links button").on("click", (e) => {
   const link = $(e.currentTarget).data("link");
   open(link, link.startsWith("mailto:") ? "_self" : "_blank");
