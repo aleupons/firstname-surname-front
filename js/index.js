8ae86993181f88a39d40ceb7f613a4fbf8a4e0f0
Aleu <apahoquei@gmail.com>
2024-10-28 22:28:21 +0100

    .

--- js/index.js
+++ js/index.js
@@ -323,7 +323,7 @@ $(".nav-link").each(function () {
   const navlink = $(this);
   navlink.on("touchend", (e) => {
     e.preventDefault();
-    window.location.href = navlink.attr("href");
+    navlink.click();
   });
 });
 
